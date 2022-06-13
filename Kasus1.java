7d93b316bfa197fd847bc976c77d16f946d28c27
Azzz <107316901+AzMy27@users.noreply.github.com>
2022-06-13 18:09:20 +0700

    Delete Kasus1.java

--- Kasus1.java
+++
@@ -1,7 +0,0 @@
-public class Kasus1 {
-    public static void main(String[] args) {
-        float c=34.1234f;
-        int b = (int)c +10;
-        System.out.println("hasil menambah c dan b adalah " +b);
-    }
-}
