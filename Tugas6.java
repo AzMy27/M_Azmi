d09171ffdad92e33832bb41ac5040931b4991ade
Azzz <107316901+AzMy27@users.noreply.github.com>
2022-06-13 18:08:49 +0700

    Delete Tugas6.java

--- Tugas6.java
+++
@@ -1,8 +0,0 @@
-public class Tugas6 {
-    public static void main(String[] args) {
-        int a = 4, b = 12;
-        //Muhammad Azmi RPL 2C
-        System.out.println(a>b);
-        System.out.println(a<b);
-    }
-}
