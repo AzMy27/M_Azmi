d12bcf0e04d810fa2e6e8e6d02084aac51fecae0
Azzz <107316901+AzMy27@users.noreply.github.com>
2022-06-13 18:07:52 +0700

    Delete Tugas2.java

--- Tugas2.java
+++
@@ -1,9 +0,0 @@
-public class Tugas2 {
-    public static void main(String[] args) {
-    int x=9,y;
-    //Muhammad Azmi RPL 2C
-        System.out.println("Nilai x Asal = "+x);
-        y=10 - ++x;
-        System.out.println("Nilai y Asal = "+y);
-    }
-}
